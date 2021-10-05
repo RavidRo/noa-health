29e93ee323004581077cd05d27988bfcc153f6bf
ravidro <ravidro@post.bgu.ac.il>
2021-10-05 21:22:50 +0300

    finished first draft

--- main.js
+++ main.js
@@ -5,8 +5,25 @@ import './styles/joinme.css';
 import './styles/about.css';
 import './styles/intro.css';
 import './styles/header.css';
+import './styles/contactform.css';
+import './styles/footer.css';
+import './styles/modal.css';
 
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
+document.querySelector('#copyright-year').innerHTML = new Date().getFullYear();
+
+var modals = document.querySelectorAll('[data-modal]');
+
+modals.forEach(function (trigger) {
+	trigger.addEventListener('click', function (event) {
+		event.preventDefault();
+		var modal = document.getElementById(trigger.dataset.modal);
+		modal.classList.add('open');
+		var exits = modal.querySelectorAll('.modal-exit');
+		exits.forEach(function (exit) {
+			exit.addEventListener('click', function (event) {
+				event.preventDefault();
+				modal.classList.remove('open');
+			});
+		});
+	});
+});
