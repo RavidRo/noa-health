c0955e0dc6230a33c0a437612891bc50124bb7f9
ravidro <ravidro@post.bgu.ac.il>
2021-10-07 11:47:29 +0300

    Connectd the config, added all the ledt fonts, adjusting to phone

--- main.js
+++ main.js
@@ -9,8 +9,44 @@ import './styles/contactform.css';
 import './styles/footer.css';
 import './styles/modal.css';
 
+// Setting the copyright's year
 document.querySelector('#copyright-year').innerHTML = new Date().getFullYear();
 
+// Connecting the config file
+import config from './config.json';
+// Header
+document.querySelector('#title').innerHTML = config.header.title;
+document.querySelector('#catch-phrase').innerHTML = config.header['catch-phrase'];
+// Intro
+document.querySelector('#quote').innerHTML = config.intro.quote;
+document.querySelector('#intro-services').innerHTML = config.intro.services.join(' | ');
+// About
+document.querySelector('#about-title').innerHTML = config.about.title;
+document.querySelector('#about-opening').innerHTML = config.about.opening;
+const paragraphsElements = config.about.paragraphs.map((paragraph) => {
+	console.log('p');
+	let p = document.createElement('p');
+	p.innerHTML = paragraph;
+	return p;
+});
+document.querySelector('#about-paragraphs').replaceChildren(...paragraphsElements);
+//Join Me
+document.querySelector('#join-title-1').innerHTML = config.joinme.title.part1;
+document.querySelector('#join-title-bold').innerHTML = config.joinme.title.boldPart;
+document.querySelector('#join-title-2').innerHTML = config.joinme.title.part2;
+document.querySelector('#join-reason-1').innerHTML = config.joinme.reason1;
+document.querySelector('#join-reason-2').innerHTML = config.joinme.reason2;
+//Help
+document.querySelector('#helps-title').innerHTML = config.helpwith.title;
+// Social Media
+document.querySelector('#social-media-title').innerHTML = config['social-media'].title;
+document.querySelector('#instagram-link').setAttribute('href', config['social-media'].instagram);
+document.querySelector('#youtube-link').setAttribute('href', config['social-media'].youtube);
+// Services
+document.querySelector('#workshop-title').innerHTML = config.services['workshop'].title;
+document.querySelector('#coaching-title').innerHTML = config.services['personal-coaching'].title;
+
+// Opening and closing the modal
 var modals = document.querySelectorAll('[data-modal]');
 
 modals.forEach(function (trigger) {
